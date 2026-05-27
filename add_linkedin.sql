092a077c88b5630bd7dc112a1070168b3aa9473a
carlos figueroa <carlosjfigueroas@gmail.com>
2026-05-27 11:51:35 -0400

    fix: add spelling instruction to avoid Bienvido typo in Ava prompt

--- add_linkedin.sql
+++
@@ -1,2 +0,0 @@
--- Run this SQL in your Supabase SQL Editor to add the linkedin_url column to your companies table
-ALTER TABLE companies ADD COLUMN linkedin_url TEXT;
--- update_limit.sql
+++
@@ -1,3 +0,0 @@
-UPDATE app_settings 
-SET value = '{"max_properties": 35, "max_photos_per_property": 10}'::jsonb 
-WHERE key = 'default_limits' AND company_id IS NULL;
